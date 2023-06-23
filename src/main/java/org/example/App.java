f6991d7685ecc0eeee785077fd1dc5b70613177e
AbdulrhmanElbadwy <a.elbadwy@itsrizzoli.it>
2023-06-23 14:00:25 +0200

    digital clock

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,43 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javax.swing.*;
+import java.util.Date;
+
+public class App {
+    static CustomFrame f = null;
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+
+        addClock();
+
+    }
+
+    static void addClock(){
+        Thread th = new Thread( () ->{
+            while (true){
+                Date now = new Date();
+                //String msg = now.toString();
+                //System.out.println(msg);
+                if (f != null){
+                    f.setDate(now);
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        th.start();
+    }
+    private static void createAndShowGUI() {
+        f = new CustomFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(750,750);
+        f.setVisible(true);
     }
 }
